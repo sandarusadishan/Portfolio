b08e936b8e25cd00978a0d38221cfd11322c4f8a
sandarusadishan <sandarusadishan0404@gmail.com>
2024-11-21 08:43:21 +0530

    new2

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["Frontend Developer,", "YouTuber,", "Web Developer,"],
+    strings: ["Fullstack Developer", "YouTuber,", "Web Developer","Mobile Application Developer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
