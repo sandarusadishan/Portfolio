5fce0594e563bc4d1918df4d89cd0df61f3b1049
sandarusadishan <sandarusadishan0404@gmail.com>
2024-11-25 01:11:16 +0530

    new10

--- main.js
+++ main.js
@@ -5,3 +5,10 @@ var typed = new Typed(".text", {
     backDelay: 1000,
     loop: true
 });
+window.onscroll = function() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.querySelector('.header').classList.add('scrolled');
+    } else {
+        document.querySelector('.header').classList.remove('scrolled');
+    }
+};
